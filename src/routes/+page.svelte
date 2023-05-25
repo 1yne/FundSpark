16e313223b93df61582d45df70cef2da5255c726
Caladan8 <tnambiar2008@gmail.com>
2023-05-25 22:29:38 +0530

    Changed landing page background and added transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<script>
+<script lang="ts">
 	import Logo from '$lib/Logo.svelte';
+	import { io } from '@svelteuidev/composables';
+	import { fly } from "svelte/transition"
+
+	let visible: boolean;
+	// @ts-ignore
+	const handleChange = ({ detail }) => {
+		visible = detail.inView;
+	};
 </script>
 
 <svelte:head>
@@ -14,19 +22,25 @@
 		</a>
 	</div>
 	<div class="w-full flex justify-center">
-		<div class="flex flex-col gap-8 max-w-[48rem] w-full text-white text-center">
-			<h1 class="text-8xl">Connecting kindness, changing lives.</h1>
-			<p class="text-xl">
-				FundSpark is a revolutionary crowdfunding platform that harnesses the power of collective
-				generosity to fuel dreams, support causes, and transform lives.
-			</p>
-			<div class="flex justify-center">
-				<a
-					href="/signup"
-					class="p-4 bg-accent text-black text-lg flex rounded-lg px-6 hover:bg-accentHover transition-all"
-					>Get Started</a
-				>
-			</div>
+		<div
+			class="flex flex-col gap-8 max-w-[48rem] w-full text-white text-center"
+			use:io={{ threshold: 1 }}
+			on:change={handleChange}
+		>
+			{#if visible}
+				<h1 class="text-8xl" in:fly={{ y: 50, duration: 500 }}>Connecting kindness, changing lives.</h1>
+				<p class="text-xl" in:fly={{ y: 50, duration: 500, delay: 200 }}>
+					FundSpark is a revolutionary crowdfunding platform that harnesses the power of collective
+					generosity to fuel dreams, support causes, and transform lives.
+				</p>
+				<div class="flex justify-center" in:fly={{ y: 50, duration: 500, delay: 400 }}>
+					<a
+						href="/signup"
+						class="p-4 bg-accent text-black text-lg flex rounded-lg px-6 hover:bg-accentHover transition-all"
+						>Get Started</a
+					>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>

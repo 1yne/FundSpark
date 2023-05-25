5fccc8d180ff55e737fc56640e527b43da3ae18d
Caladan8 <tnambiar2008@gmail.com>
2023-05-25 22:47:12 +0530

    Added back side and top navbar

---
+++ src/lib/components/TopNavbar.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import { userStore } from '$lib/stores/UserStore';
+	import UserAvatarFilledAlt from 'carbon-icons-svelte/lib/UserAvatarFilledAlt.svelte';
+	import Search from 'carbon-icons-svelte/lib/Search.svelte';
+	import { Paper, Tooltip } from '@svelteuidev/core';
+
+	function openModal() {
+		(document.querySelector('.modal') as HTMLDialogElement).showModal();
+	}
+	function closeModal(event: any) {
+		console.log(event.target.classList);
+		const modal = document.querySelector('.modal') as HTMLDialogElement;
+		if (
+			event.target.classList.contains('closeButton') ||
+			event.target.classList.contains('modal')
+		) {
+			modal.classList.add('closingAnimation');
+			modal.addEventListener(
+				'animationend',
+				() => {
+					modal.classList.remove('closingAnimation');
+					modal.close();
+				},
+				{ once: true }
+			);
+		}
+	}
+</script>
+
+<div class="flex topNavbar justify-between bg-[#2d3134] px-4 py-2">
+	<div class="flex items-center py-1">
+		<Paper
+			override={{ backgroundColor: '#111117', py: '8px', px: "12px", borderRadius: '8px' }}
+			class="hover:bg-searchHover transition-all"
+		>
+			<div class="flex gap-4 items-center">
+				<Search size={20} fill="white" />
+				<input class="outline-none text-white min-w-[20rem] bg-transparent" placeholder="Search..." />
+			</div>
+		</Paper>
+	</div>
+	<div class="flex gap-4 items-center">
+		<div class="py-1 h-full flex justify-center items-center">
+			<Tooltip label={`${$userStore?.created_projects.length === 0 ? "Create your first FundWave" : "New FundWave"}`} override={{ height: "100%"}}>
+				<a
+					href="/dashboard/create"
+					class="w-full h-full bg-mint rounded-lg px-4 text-sm hover:bg-mint/80 transition-all flex justify-center items-center"
+					>Start a campaign</a
+				>
+			</Tooltip>
+		</div>
+		<a href="/dashboard/profile">
+			{#if $userStore?.profile_picture}
+				<img
+					src={$userStore.profile_picture}
+					alt="profile"
+					class="rounded-full flex w-[52px] h-[52px]"
+				/>
+			{:else}
+				<div class="">
+					<UserAvatarFilledAlt size={36} fill="white" />
+				</div>
+			{/if}
+		</a>
+	</div>
+</div>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<dialog class="modal p-0 bg-blackBackground text-white" id="modal" on:click={closeModal}>
+	<div class="modal-wrapper p-4 w-full h-full">
+		<h1 class="text-4xl font-bold">Create your campaign</h1>
+		<button on:click={closeModal} class="closeButton">Close</button>
+	</div>
+</dialog>
+
+<style>
+	.modal::backdrop {
+		background-color: rgb(2 195 154 / 0.1);
+		opacity: 0;
+	}
+
+	.modal[open]::backdrop {
+		animation: fade-in 500ms forwards;
+	}
+
+	.modal[open] {
+		animation: slide-in 500ms forwards;
+		width: calc(100% - 6rem);
+		height: calc(100vh - 6rem);
+		border-radius: 8px;
+	}
+
+	@keyframes slide-in {
+		0% {
+			transform: translateY(100%);
+		}
+		100% {
+			transform: translateY(0%);
+		}
+	}
+
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+</style>
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,21 +1,52 @@
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
+	import SideNavbar from '$lib/components/SideNavbar.svelte';
+	import TopNavbar from '$lib/components/TopNavbar.svelte';
 	import { fly } from 'svelte/transition';
+	import { page } from '$app/stores';
 
 	export let data: import('./$types').LayoutData;
 </script>
 
 <div class="flex h-full w-full bg-[#13121a] flex-col">
-	<Navbar />
-	<div class="content text-white p-6">
-		{#key data.url}
-			<div
-				in:fly={{ x: -5, duration: 300, delay: 300 }}
-				out:fly={{ x: -5, duration: 300 }}
-				class="w-full h-full"
-			>
-				<slot />
+	<div class="flex h-full w-full">
+		{#if $page.route.id === '/dashboard'}
+			<div in:fly={{ x: -20, duration: 250, delay: 300 }} out:fly={{ x: -20, duration: 250 }} class="h-full">
+				<SideNavbar />
 			</div>
-		{/key}
+			<div class="flex flex-col w-full h-full">
+				<div in:fly={{ y: -20, duration: 250, delay: 300 }} out:fly={{ y: -20, duration: 250 }} class="w-full">
+					<TopNavbar  />
+				</div>
+				<div class="content text-white p-6">
+					{#key data.url}
+						<div
+							in:fly={{ x: -5, duration: 300, delay: 300 }}
+							out:fly={{ x: -5, duration: 300 }}
+							class="w-full h-full"
+						>
+							<slot />
+						</div>
+					{/key}
+				</div>
+			</div>
+		{:else}
+			<div class="flex flex-col w-full h-full">
+				<div in:fly={{ y: -20, duration: 250, delay: 300 }} out:fly={{ y: -20, duration: 250 }}>
+					<Navbar />
+				</div>
+				<div class="content text-white p-6">
+					{#key data.url}
+						<div
+							in:fly={{ x: -5, duration: 300, delay: 300 }}
+							out:fly={{ x: -5, duration: 300 }}
+							class="w-full h-full"
+						>
+							<slot />
+						</div>
+					{/key}
+				</div>
+			</div>
+		{/if}
 	</div>
 </div>

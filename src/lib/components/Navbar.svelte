3b3f3f2bffb8bd3afc2622bade5bd681aa70e8ac
Caladan8 <tnambiar2008@gmail.com>
2023-05-30 21:59:45 +0530

    Remove unused code

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,28 +8,6 @@
 	import WalletIcon from "$lib/WalletIcon.svelte"
 
 	let searchInputFocused = false
-
-	function openModal() {
-		(document.querySelector('.modal') as HTMLDialogElement).showModal();
-	}
-	function closeModal(event: any) {
-		console.log(event.target.classList);
-		const modal = document.querySelector('.modal') as HTMLDialogElement;
-		if (
-			event.target.classList.contains('closeButton') ||
-			event.target.classList.contains('modal')
-		) {
-			modal.classList.add('closingAnimation');
-			modal.addEventListener(
-				'animationend',
-				() => {
-					modal.classList.remove('closingAnimation');
-					modal.close();
-				},
-				{ once: true }
-			);
-		}
-	}
 </script>
 
 <div class="topNavbar justify-between bg-navbarBG px-4 py-2 fixed w-full">
@@ -79,49 +57,7 @@
 	</div>
 </div>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<dialog class="modal p-0 bg-blackBackground text-white" id="modal" on:click={closeModal}>
-	<div class="modal-wrapper p-4 w-full h-full">
-		<h1 class="text-4xl font-bold">Create your {$userStore?.created_projects.length === 0 ? "first" : ""} FundWave</h1>
-		<button on:click={closeModal} class="closeButton">Close</button>
-	</div>
-</dialog>
-
 <style>
-	.modal::backdrop {
-		background-color: rgb(2 195 154 / 0.1);
-		opacity: 0;
-	}
-
-	.modal[open]::backdrop {
-		animation: fade-in 500ms forwards;
-	}
-
-	.modal[open] {
-		animation: slide-in 500ms forwards;
-		width: calc(100% - 6rem);
-		height: calc(100vh - 6rem);
-		border-radius: 8px;
-	}
-
-	@keyframes slide-in {
-		0% {
-			transform: translateY(100%);
-		}
-		100% {
-			transform: translateY(0%);
-		}
-	}
-
-	@keyframes fade-in {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
-
 	.topNavbar {
 		display: grid;
 		grid-template-columns: 1fr auto 1fr;

25fd44c3a522d32904ddce76f6daba9c513e22a6
Caladan8 <tnambiar2008@gmail.com>
2023-06-03 16:00:02 +0530

    Fetch matching FundWaves in /search load

--- src/routes/dashboard/search/+page.server.ts
+++ src/routes/dashboard/search/+page.server.ts
@@ -1,5 +1,12 @@
 import type { PageServerLoad } from './$types';
+import { supabase } from "$lib/supabase.ts"
 
 export const load: PageServerLoad = async ({ url }) => {
-  const searchQuery = url.searchParams.get("q")
+	const searchQuery = url.searchParams.get('q');
+
+	const { data, error } = await supabase.from('FundWaves').select().like('name', `%${searchQuery}%`);
+  if (error) {
+    console.log('error: ', error);
+  }
+  console.log(data)
 };

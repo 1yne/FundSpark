4d4dd235cfe74a8b5f876b67a2c0e8eb042018fd
Caladan8 <tnambiar2008@gmail.com>
2023-06-03 16:24:29 +0530

    Return matches to /search page

--- src/routes/dashboard/search/+page.server.ts
+++ src/routes/dashboard/search/+page.server.ts
@@ -4,9 +4,14 @@ import { supabase } from "$lib/supabase.ts"
 export const load: PageServerLoad = async ({ url }) => {
 	const searchQuery = url.searchParams.get('q');
 
-	const { data, error } = await supabase.from('FundWaves').select().like('name', `%${searchQuery}%`);
+	const { data: matches, error } = await supabase.from('FundWaves').select().like('name', `%${searchQuery}%`);
   if (error) {
     console.log('error: ', error);
   }
-  console.log(data)
+  console.log(matches)
+
+  return {
+    searchQuery,
+    matches
+  }
 };
--- src/routes/dashboard/search/+page.svelte
+++ src/routes/dashboard/search/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <title>Search Results: {data.searchQuery}</title>
+</svelte:head>
+
 <h1>Search Results: </h1>

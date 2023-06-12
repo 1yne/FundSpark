7c28a774c5a635e7067b2e749bdddfdd89ea645b
Caladan8 <tnambiar2008@gmail.com>
2023-06-12 22:03:41 +0530

    Added SearchResultCard

---
+++ src/lib/components/SearchResultCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Paper } from "@svelteuidev/core"
+  export let title: string, description: string
+</script>
+
+<Paper override={{ backgroundColor: "#1c1c25", color: "white", padding: 0 }}>
+  <div class="content p-4">
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </div>
+</Paper>
--- src/routes/dashboard/search/+page.svelte
+++ src/routes/dashboard/search/+page.svelte
@@ -1,9 +1,20 @@
 <script>
+	// @ts-nocheck
+
+	import { SimpleGrid } from "@svelteuidev/core"
+	import SearchResultCard from "$lib/components/SearchResultCard.svelte";
 	export let data;
+
+	$: console.log(data.matches)
 </script>
 
 <svelte:head>
 	<title>Search Results: {data.searchQuery}</title>
 </svelte:head>
 
-<h1>Search results for {data.searchQuery} ({data.matches?.length})</h1>
+<h1 class="mb-4">Search results for {data.searchQuery} ({data.matches?.length})</h1>
+<SimpleGrid cols={4}>
+	{#each data.matches as searchResult}
+		<SearchResultCard title={searchResult.name} description={searchResult.description} />
+	{/each}
+</SimpleGrid>

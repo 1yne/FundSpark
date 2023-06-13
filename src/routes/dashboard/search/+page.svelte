3fb2a7735a72c659fcb3be8498eb053995b3b89a
Caladan8 <tnambiar2008@gmail.com>
2023-06-13 22:46:38 +0530

    Change size of search results display text

--- src/routes/dashboard/search/+page.svelte
+++ src/routes/dashboard/search/+page.svelte
@@ -10,7 +10,7 @@
 	<title>Search Results: {data.searchQuery}</title>
 </svelte:head>
 
-<h1 class="mb-4">Search results for {data.searchQuery} ({data.matches?.length})</h1>
+<h1 class="mb-4 text-lg">Search results for {data.searchQuery} ({data.matches?.length})</h1>
 <SimpleGrid cols={4}>
 	{#each data.matches as searchResult}
 		<SearchResultCard title={searchResult.name} description={searchResult.description} id={searchResult.id} />

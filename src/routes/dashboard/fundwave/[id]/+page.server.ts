d58d6fa89c572435a64b245d445654fb23d3f0ee
Caladan8 <tnambiar2008@gmail.com>
2023-06-14 22:33:46 +0530

    Display FW name in title

--- src/routes/dashboard/fundwave/[id]/+page.server.ts
+++ src/routes/dashboard/fundwave/[id]/+page.server.ts
@@ -2,10 +2,10 @@ import { supabase } from '$lib/supabase.ts';
 import type { FundWave } from "$lib/types.ts"
 
 export const load = async ({ params }) => {
-	const { data: fundwave, error }: { data: FundWave } = await supabase.from('FundWaves').select().eq('id', params.id);
+	const { data: fundwave, error }: { data: FundWave[] } = await supabase.from('FundWaves').select().eq('id', params.id);
 	if (error) console.log(error);
 
   return {
-    fundwave
+    fundwave: fundwave[0]
   }
 };
--- src/routes/dashboard/fundwave/[id]/+page.svelte
+++ src/routes/dashboard/fundwave/[id]/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+  export let data
+</script>
+
+<svelte:head>
+  <title>{data.fundwave.name}</title>
+</svelte:head>
+
 <h1>Page</h1>

0856b4aa5245cab3d2a4321c41073f64de39c323
Caladan8 <tnambiar2008@gmail.com>
2023-06-14 22:31:40 +0530

    Add type declaration for matching FW in /fundwave/id

--- src/routes/dashboard/fundwave/[id]/+page.server.ts
+++ src/routes/dashboard/fundwave/[id]/+page.server.ts
@@ -1,7 +1,8 @@
 import { supabase } from '$lib/supabase.ts';
+import type { FundWave } from "$lib/types.ts"
 
 export const load = async ({ params }) => {
-	const { data: fundwave, error } = await supabase.from('FundWaves').select().eq('id', params.id);
+	const { data: fundwave, error }: { data: FundWave } = await supabase.from('FundWaves').select().eq('id', params.id);
 	if (error) console.log(error);
 
   return {
